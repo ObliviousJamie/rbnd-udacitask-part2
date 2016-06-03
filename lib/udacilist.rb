3dffe32fc826e4a523ca72a5166bb3a78b57af4a
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-06-03 13:19:28 +0100

    Chronic now used for dates and extra error checking added

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -6,14 +6,19 @@ class UdaciList
     @items = []
   end
   def add(type, description, options={})
+    adding_error_check(type, options)
+
     type = type.downcase
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
   end
+
   def delete(index)
-    @items.delete_at(index - 1)
+      deleting_error_check(index)
+      @items.delete_at(index - 1)
   end
+
   def all
     puts "-" * @title.length
     puts @title
@@ -21,5 +26,21 @@ class UdaciList
     @items.each_with_index do |item, position|
       puts "#{position + 1}) #{item.details}"
     end
+  end
+
+  private
+
+  def adding_error_check(type, options = {})
+    valid_type =  ["todo","event","link"].any? {|type_item| type_item == type}
+    raise UdaciListErrors::InvalidItemType, "#{type} is not recognised" if !valid_type
+
+    if options[:priority]
+        valid_priority = ["low","medium","high"].any? {|priority| priority == options[:priority]}
+        raise UdaciListErrors::InvalidPriorityValue, "Priority #{options[:priority]} not recognised" if !valid_priority
+    end
+  end
+
+  def deleting_error_check(index)
+      raise UdaciListErrors::IndexExceedsListSize, "Entry at index:#{index} not found" if (@items.length < index) 
   end
 end
